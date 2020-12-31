9d967dcfbbf7a3dd47f775fa36d8e3eabb213bdf
Spring-ch <2350824600@qq.com>
2021-01-01 00:30:16 +0800

    批量导出svg文件，并封装Icon组件，方便后续svg的引入

---
+++ src/components/Icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="icon" aria-hidden="true">
+    <use :xlink:href="'#' + name" />
+  </svg>
+</template>
+<script lang="ts">
+import Vue from "vue";
+// 整体导入svg文件，不需要单独import文件
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) =>
+  requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context("../assets/icon", true, /\.svg$/));
+} catch (e) {
+  console.log(e);
+}
+
+export default Vue.extend({
+  props: ["name"],
+});
+</script>
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,28 +1,13 @@
 <template>
   <div>
-    <router-link to="/labels">标签</router-link> |
-    <router-link to="/money"
-      >记账
-      <svg class="icon" aria-hidden="true">
-        <use xlink:href="#money" />
-      </svg>
-    </router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/labels">标签<Icon name="label" /> </router-link>|
+    <router-link to="/money">记账 <Icon name="money" /></router-link>|
+    <router-link to="/statistics">统计<Icon name="static" /></router-link>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
-import x from "@/assets/icon/money.svg";
-console.log(x);
 export default Vue.extend({});
 </script>
 <style lang="scss" scoped>
-.icon {
-  width: 1em;
-  height: 1em;
-  vertical-align: -0.15em;
-  fill: currentColor;
-  overflow: hidden;
-}
 </style>
